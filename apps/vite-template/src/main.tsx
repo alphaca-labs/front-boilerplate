6edd68c50690c835d9777c4ce64e572dbeb3bf86
mj <minai621@naver.com>
2024-06-27 20:00:28 +0900

    chore: pandacss 설정

--- apps/vite-template/src/main.tsx
+++ apps/vite-template/src/main.tsx
@@ -1,25 +1,20 @@
 import App from "@/App";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./global.css";
 
 async function enableMocking() {
   if (import.meta.env.MODE === "production") {
     return;
   }
   const { worker } = await import("@/mock-api/browser");
-  console.log("Mocking worker loaded", worker);
-
-  // `worker.start()` returns a Promise that resolves
-  // once the Service Worker is up and ready to intercept requests.
-  console.log("Starting the mocking worker");
   return worker.start();
 }
 
 enableMocking().then(() => {
-  console.log("run app");
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
+    </React.StrictMode>
   );
 });
