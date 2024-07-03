05b3abb93277d12a3e0bc886c5681c6aa0f1d397
mj <minai621@naver.com>
2024-07-03 18:40:25 +0900

    feat: icon-generator scripts 수정

--- scripts/icon-generator.ts
+++ scripts/icon-generator.ts
@@ -4,34 +4,34 @@ import { fileURLToPath } from "url";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
-const iconsJsonPath = path.resolve(
+const iconsDirPath = path.resolve(__dirname, "..", "packages", "icon", "icons");
+const outputBaseDir = path.resolve(
   __dirname,
   "..",
   "packages",
-  "icons",
-  "icons.json"
-);
-
-const outputDir = path.resolve(
-  __dirname,
-  "..",
-  "packages",
-  "icons",
+  "icon",
   "src",
-  "components",
-  "icons"
+  "components"
 );
 
-if (!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir, { recursive: true });
-}
+const ensureDirectoryExists = (dirPath: string) => {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+  }
+};
 
-const icons = JSON.parse(fs.readFileSync(iconsJsonPath, "utf-8"));
+const toPascalCase = (str: string) => {
+  return str.replace(/(^\w|-\w)/g, (match) =>
+    match.replace("-", "").toUpperCase()
+  );
+};
 
-const createComponent = (iconId: string, svgContent: string) => {
-  const componentName = iconId
-    .replace(/-([a-z])/g, (g) => g[1].toUpperCase())
-    .replace(/^./, (str) => str.toUpperCase());
+const createComponent = (
+  dirName: string,
+  iconId: string,
+  svgContent: string
+) => {
+  const componentName = toPascalCase(iconId);
 
   svgContent = svgContent.replace(/stroke="black"/g, 'stroke="currentColor"');
   svgContent = svgContent.replace(/fill="none"/g, 'fill="currentColor"');
@@ -46,12 +46,40 @@ const ${componentName} = forwardRef((props, ref: ForwardedRef<SVGSVGElement>) =>
 export default ${componentName};
   `;
 
+  const outputDir = path.join(outputBaseDir, dirName);
+  ensureDirectoryExists(outputDir);
+
   fs.writeFileSync(
     path.join(outputDir, `${componentName}.tsx`),
     componentTemplate
   );
+
+  return componentName;
 };
 
-Object.entries(icons).forEach(([key, value]) => {
-  createComponent(key, (value as any).svg);
-});
+const generateComponents = () => {
+  const jsonFiles = fs
+    .readdirSync(iconsDirPath)
+    .filter((file) => file.endsWith(".json"));
+
+  let indexContent = "";
+
+  jsonFiles.forEach((file) => {
+    const filePath = path.join(iconsDirPath, file);
+    const icons = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    const dirName = path.basename(file, path.extname(file));
+
+    Object.entries(icons).forEach(([key, value]) => {
+      const componentName = createComponent(dirName, key, (value as any).svg);
+      indexContent += `export { default as ${componentName} } from './${dirName}/${componentName}';\n`;
+    });
+  });
+
+  const indexPath = path.join(outputBaseDir, "index.ts");
+  ensureDirectoryExists(path.dirname(indexPath));
+  fs.writeFileSync(indexPath, indexContent);
+
+  console.log("SVG 컴포넌트 및 인덱스 파일 생성 완료!");
+};
+
+generateComponents();
